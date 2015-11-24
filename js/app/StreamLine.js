6d2b146ed0c53e91fc6081e841fb10e01fa97bd3
victor <zhanchangwen@live.com>
2015-11-24 22:13:12 +0800

    修改着色器，实现绘制，但是没有实现流线功能

--- js/app/StreamLine.js
+++ js/app/StreamLine.js
@@ -2,7 +2,7 @@
  * Created by work on 2015/11/19.
  */
 define(["app/ShaderProgram","app/NoiseFrame","app/Vector","lib/glMatrix-0.9.5.min"],function(ShaderProgram,NoiseFrame,Vector){
-    var StreamLine = function(){
+    var StreamLine = function(gl){
         console.log(Vector);
         var shaderProgram = new ShaderProgram(gl,"streamline");
         //将attribute、uniform位置绑定到shaderProgram上
@@ -13,19 +13,19 @@ define(["app/ShaderProgram","app/NoiseFrame","app/Vector","lib/glMatrix-0.9.5.mi
         var texture = noiseFrame.texture;
         bindingLocation(gl,shaderProgram);
         this.bindBuffers = function(){
-            gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+            gl.enableVertexAttribArray(shaderProgram.vertexVectorAttribute);
             gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
             gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, buffers.position.itemSize, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
-            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.coord);
-            gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, buffers.coord.itemSize, gl.FLOAT, false, 0, 0);
+            //gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+            //gl.bindBuffer(gl.ARRAY_BUFFER, buffers.coord);
+            //gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, buffers.coord.itemSize, gl.FLOAT, false, 0, 0);
             gl.activeTexture(gl.TEXTURE0);
             gl.bindTexture(gl.TEXTURE_2D,texture);
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,buffers.index);
         };
         this.setUniforms = function(){
-            gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, uniforms.pMatrix);
-            gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, uniforms.mvMatrix);
+            //gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, uniforms.pMatrix);
+            //gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, uniforms.mvMatrix);
             gl.uniform1i(shaderProgram.uSamplerUniform,false,0);
         };
         this.draw = function(){
@@ -39,40 +39,58 @@ define(["app/ShaderProgram","app/NoiseFrame","app/Vector","lib/glMatrix-0.9.5.mi
     //将着色器中attribute、uniform位置绑定到shaderProgram对象上
     //不同的着色器处理方式有差异，同时会影响到draw
     function bindingLocation(gl,shaderProgram){
-        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram,"aVertexPosition");
-        shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram,"aTextureCoord");
-        shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-        shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+        shaderProgram.vertexVectorAttribute = gl.getAttribLocation(shaderProgram,"aVertexVector");
+        //shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram,"aTextureCoord");
+        //shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+        //shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
         shaderProgram.uSamplerUniform = gl.getUniformLocation(shaderProgram,"uSampler");
     }
     function createBuffers(gl){
         var buffers = {};
-        var positions = [
-            -1.0, -1.0,  0.0,
-            1.0, -1.0,  0.0,
-            1.0,  1.0,  0.0,
-            -1.0,  1.0, 0.0
-        ];
-        var coords = [
-            0.0, 0.0,
-            1.0, 0.0,
-            1.0, 1.0,
-            0.0, 1.0
-        ];
-        var indices = [0,1,2,0,2,3];
-        buffers.position = createBuffer(gl,positions,3);
-        buffers.coord = createBuffer(gl,coords,2);
+        //var positions = [
+        //    -1.0, -1.0,  0.0,
+        //    1.0, -1.0,  0.0,
+        //    1.0,  1.0,  0.0,
+        //    -1.0,  1.0, 0.0
+        //];
+        //var coords = [
+        //    0.0, 0.0,
+        //    1.0, 0.0,
+        //    1.0, 1.0,
+        //    0.0, 1.0
+        //];
+        var width = 31;
+        var height = 31;
+        var indices = createIndices(width,height);
+        buffers.position = createBuffer(gl,Vector,4);
+        //buffers.coord = createBuffer(gl,coords,2);
         buffers.index = createElementBuffer(gl,indices);
         return buffers;
     }
+    function createIndices(width,height){
+        var indices = [];
+        for(var i=0;i<height-1;i++){
+            for(var j=0;j<width-1;j++){
+                var first = i * width;
+                var second = first + width;
+                indices.push(second);
+                indices.push(first);
+                indices.push(second+1);
+                indices.push(second+1);
+                indices.push(first);
+                indices.push(first+1);
+            }
+        }
+        return indices;
+    }
     function createUniforms(gl){
         var uniforms = {};
-        uniforms.mvMatrix = mat4.create();
-        uniforms.pMatrix = mat4.create();
-        mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, uniforms.pMatrix);
-        mat4.identity(uniforms.mvMatrix);
-
-        mat4.translate(uniforms.mvMatrix, [-0.0, 0.0, -7.0]);
+        //uniforms.mvMatrix = mat4.create();
+        //uniforms.pMatrix = mat4.create();
+        //mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, uniforms.pMatrix);
+        //mat4.identity(uniforms.mvMatrix);
+        //
+        //mat4.translate(uniforms.mvMatrix, [-0.0, 0.0, -7.0]);
         return uniforms;
     }
     function createBuffer(gl,values,itemSize){
